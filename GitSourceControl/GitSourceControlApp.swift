8d0eacce9a6b05d729038cccc82829575bb28d46
Nina <nina.fyodorowa@gmail.com>
2026-01-18 23:20:30 +0100

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by Нина Федорова on 2026. 01. 18..
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

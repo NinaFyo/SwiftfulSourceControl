5ab7750bf9a6aaa3873d34bbd2fc6304bfbe0678
NinaFyo <nina.fyodorowa@gmail.com>
2026-01-19 01:30:18 +0100

    [Clean] Add commit message documentation

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by Нина Федорова on 2026. 01. 18..
 //
 
+/*
+ COMMIT MESSAGES
+ depending on what you are building, add a prefix to it
+ 
+ Examples:
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] (means patch smth that's already out) Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Fix this bug, Description of the bug
+ 
+ MUNDANE TASKS: (cleaning or organizing codebase, renaming things or moving files around)
+ [Clean] Description of changes
+ 
+ RELEASE COMMITS (push smth to production)
+ [release] Descrption of release
+ */
+
 import SwiftUI
 
 struct ContentView: View {

70da8ae7d0fa994b73d8167a775879c48c27d556
NinaFyo <nina.fyodorowa@gmail.com>
2026-01-19 01:10:29 +0100

    Add HomeView

---
+++ GitSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitSourceControl
+//
+//  Created by Нина Федорова on 2026. 01. 19..
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}

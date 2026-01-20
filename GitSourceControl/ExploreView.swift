6e6e42507a95ca184ec73c9dae279aef760dc27f
NinaFyo <nina.fyodorowa@gmail.com>
2026-01-20 19:35:23 +0100

    [Feature] Add ExploreView

---
+++ GitSourceControl/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  GitSourceControl
+//
+//  Created by Нина Федорова on 2026. 01. 20..
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
